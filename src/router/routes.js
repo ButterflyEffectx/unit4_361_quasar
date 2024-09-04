97f807ea3bf6e9afe6da8b53e38cc4a742db231a
overdose. <onyoux755@gmail.com>
2024-09-04 13:15:32 +0700

    unit-test-majestic

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,8 +3,20 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      {
+        path: '',
+        component: () => import('pages/IndexPage.vue')
+      },
+      {
+        path: '/Input',
+        component: () => import('pages/InputPage.vue')
+      },
+      {
+        path: '/Function',
+        component: () => import('pages/FunctionPage.vue')
+      },
+    ],
+
   },
 
   // Always leave this as last one,
