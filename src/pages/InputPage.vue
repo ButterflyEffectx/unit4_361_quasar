4e1820b0e894872dea224ed77b45db8f1cb45c07
overdose. <onyoux755@gmail.com>
2024-09-04 13:46:40 +0700

    !add page and manage router.

--- src/pages/InputPage.vue
+++ src/pages/InputPage.vue
@@ -4,6 +4,8 @@
     <form>
       <input type="text" id="firstName" v-model="firstName" />
       <input type="text" id="secondName" v-model="secondName" />
+      <input type="text" id="nickname" v-model="nickname" />
+      <input type="text" id="studentcode" v-model="studentcode" />
       <button @click="submitForm()" class="submitForm">Submit</button>
   </form>
   </div>
@@ -14,9 +16,11 @@ export default {
   name: 'InputPage',
   data() {
     return {
-      title: 'Vue is awesome.',
-      firstName: '',
-      secondName: '',
+      title: 'Input Page.',
+      firstName: 'Peeraphat',
+      secondName: 'Micranuch',
+      nickname: 'Fook',
+      studentcode: '6604101361',
     }
   },
   methods: {
@@ -27,4 +31,51 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+
+  h1 {
+    text-align: center;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: white;
+    padding: 20px;
+    border-radius: 10px;
+  }
+
+  input[type="text"] {
+    width: 100%;
+    max-width: 300px;
+    padding: 10px;
+    margin: 10px 0;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    font-size: 16px;
+  }
+
+  button.submitForm {
+    width: 100%;
+    max-width: 300px;
+    padding: 10px;
+    margin-top: 20px;
+    background-color: #00a6e7;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    font-size: 18px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+  }
+
+  button.submitForm:hover {
+    background-color: #008ac0;
+  }
+
+  button.submitForm:active {
+    background-color: #006f9b;
+  }
+
+</style>
